51f85141d518194264c1fabf2484cc10498c5fb3
Thiago Rocha <thiagodesousarocha1311@gmail.com>
2022-11-04 12:40:09 -0300

    criação do header e inicio do banner

---
+++ script.js
@@ -0,0 +1,9 @@
+const btnMobile = document.querySelector('#btn-mobile');
+
+function toggleMenu(){
+const nav = document.querySelector('.nav-header');
+nav.classList.toggle('active');
+
+}
+
+btnMobile.addEventListener('click', toggleMenu);
